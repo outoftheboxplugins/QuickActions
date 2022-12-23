a05627388e468d48059adbe0160849acc808b4b1
Oprea Alexandru <oprea_alexandrum@yahoo.com>
2022-12-23 15:50:09 +0200

    created base class for extensions

---
+++ Source/SpotlightSearch/Private/QuickMenuExtension.h
@@ -0,0 +1,35 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "QuickMenuExtension.generated.h"
+
+struct FQuickCommandEntry
+{
+	// TODO: Check if we need to default the move constructor as well
+	FQuickCommandEntry() = default;
+	FQuickCommandEntry(const TSharedRef<FUICommandInfo>& Command, const TSharedRef<FUICommandList> CommandList);
+
+	TAttribute<FText> Title;
+	TAttribute<FText> Tooltip;
+
+	FSlateIcon Icon;
+
+	FSimpleDelegate ExecuteCallback;
+	// TODO: Change this delegate type to something nice like: FCanExecuteAction
+	TDelegate<bool()> CanExecuteCallback;
+};
+
+/**
+ *
+ */
+UCLASS(Abstract, Blueprintable)
+class SPOTLIGHTSEARCH_API UQuickMenuExtension : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION()
+	virtual TArray<FQuickCommandEntry> GetCommands() PURE_VIRTUAL(return {};)
+};

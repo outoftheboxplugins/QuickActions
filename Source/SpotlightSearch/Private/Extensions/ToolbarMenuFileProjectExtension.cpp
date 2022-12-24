da2c6eaebce9b789cc3959e0b1e6599bf116c8c7
Oprea Alexandru <oprea_alexandrum@yahoo.com>
2022-12-24 16:53:49 +0200

    added project section of the main menu toolbar

---
+++ Source/SpotlightSearch/Private/Extensions/ToolbarMenuFileProjectExtension.cpp
@@ -0,0 +1,37 @@
+﻿// Copyright Out-of-the-Box Plugins 2018-2021. All Rights Reserved.
+
+#include "ToolbarMenuFileProjectExtension.h"
+
+#include <Interfaces/IMainFrameModule.h>
+
+TArray<FQuickCommandEntry> UToolbarMenuFileProjectExtension::GetCommands()
+{
+	TArray<FQuickCommandEntry> OutCommands;
+
+	IMainFrameModule& MainFrameModule = FModuleManager::Get().LoadModuleChecked<IMainFrameModule>("MainFrame");
+	TSharedPtr<const FUICommandList> MainFrameCommands = MainFrameModule.GetMainFrameCommandBindings();
+
+	UToolMenus* ToolMenus = UToolMenus::Get();
+	UToolMenu* MainTabFileMenu = ToolMenus->ExtendMenu("MainFrame.MainTabMenu.File");
+	FToolMenuSection* ProjectSection = MainTabFileMenu->FindSection("FileProject");
+
+	for (FToolMenuEntry& Block : ProjectSection->Blocks)
+	{
+		// NOTE: the FileSave block has no command list assigned, we temporarily assign the MainFrame list to access the FUIAction and revert it afterwards
+		Block.SetCommandList(MainFrameCommands);
+		if (const FUIAction* FoundAction = Block.GetActionForCommand(ProjectSection->Context, MainFrameCommands))
+		{
+			FQuickCommandEntry MenuEntry;
+			MenuEntry.Title = Block.Label;
+			MenuEntry.Tooltip = Block.ToolTip;
+			MenuEntry.Icon = Block.Icon;
+			MenuEntry.CanExecuteCallback = FoundAction->CanExecuteAction;
+			MenuEntry.ExecuteCallback = FoundAction->ExecuteAction;
+
+			OutCommands.Emplace(MenuEntry);
+		}
+		Block.SetCommandList({});
+	}
+
+	return OutCommands;
+}
---
+++ Source/SpotlightSearch/Private/Extensions/ToolbarMenuFileProjectExtension.h
@@ -0,0 +1,18 @@
+﻿// Copyright Out-of-the-Box Plugins 2018-2021. All Rights Reserved.
+
+#pragma once
+
+#include "QuickMenuExtension.h"
+
+#include "ToolbarMenuFileProjectExtension.generated.h"
+
+/**
+ * Add entries from the main frame toolbar -> File menu -> Project section
+ */
+UCLASS()
+class UToolbarMenuFileProjectExtension : public UQuickMenuExtension
+{
+	GENERATED_BODY()
+
+	virtual TArray<FQuickCommandEntry> GetCommands() override;
+};

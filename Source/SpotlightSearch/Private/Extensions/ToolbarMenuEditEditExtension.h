b2bef38ccb69954aec1e2e7e808644fe4af3e3b9
Oprea Alexandru <oprea_alexandrum@yahoo.com>
2022-12-24 20:03:17 +0200

    base classes for edit->edit

---
+++ Source/SpotlightSearch/Private/Extensions/ToolbarMenuEditEditExtension.h
@@ -0,0 +1,18 @@
+﻿// Copyright Out-of-the-Box Plugins 2018-2021. All Rights Reserved.
+
+#pragma once
+
+#include "QuickMenuExtension.h"
+
+#include "ToolbarMenuEditEditExtension.generated.h"
+
+/**
+ * Add entries from the main frame toolbar -> Edit menu -> Edit section
+ */
+UCLASS()
+class UToolbarMenuEditEditExtension : public UQuickMenuExtension
+{
+	GENERATED_BODY()
+
+	virtual TArray<FQuickCommandEntry> GetCommands() override;
+};

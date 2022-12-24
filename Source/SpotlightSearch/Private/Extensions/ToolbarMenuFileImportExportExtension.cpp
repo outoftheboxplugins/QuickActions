f473df725acd3e46deef49aec119630ee0048fe4
Oprea Alexandru <oprea_alexandrum@yahoo.com>
2022-12-24 11:08:58 +0200

    added import/export commands

--- Source/SpotlightSearch/Private/Extensions/ToolbarMenuFileImportExportExtension.cpp
+++ Source/SpotlightSearch/Private/Extensions/ToolbarMenuFileImportExportExtension.cpp
@@ -2,9 +2,24 @@
 
 #include "ToolbarMenuFileImportExportExtension.h"
 
+#include <LevelEditor.h>
+#include <LevelEditorActions.h>
+
 TArray<FQuickCommandEntry> UToolbarMenuFileImportExportExtension::GetCommands()
 {
 	TArray<FQuickCommandEntry> OutCommands;
+
+	const FLevelEditorModule& LevelEditorModule = FModuleManager::Get().LoadModuleChecked<FLevelEditorModule>("LevelEditor");
+	const TSharedRef<FUICommandList> LevelEditorCommands = LevelEditorModule.GetGlobalLevelEditorActions();
+
+	FQuickCommandEntry ImportScene = FQuickCommandEntry(FLevelEditorCommands::Get().ImportScene.ToSharedRef(), LevelEditorCommands);
+	OutCommands.Emplace(ImportScene);
+
+	FQuickCommandEntry ExportAll = FQuickCommandEntry(FLevelEditorCommands::Get().ExportAll.ToSharedRef(), LevelEditorCommands);
+	OutCommands.Emplace(ExportAll);
+
+	FQuickCommandEntry ExportSelected = FQuickCommandEntry(FLevelEditorCommands::Get().ExportSelected.ToSharedRef(), LevelEditorCommands);
+	OutCommands.Emplace(ExportSelected);
 
 	return OutCommands;
 }

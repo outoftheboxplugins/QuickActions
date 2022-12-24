ec786bf617c774bb49824dfe80c0d9734a2cede1
Oprea Alexandru <oprea_alexandrum@yahoo.com>
2022-12-24 07:38:23 +0200

    created base structure for import/export menu

---
+++ Source/SpotlightSearch/Private/Extensions/ToolbarMenuFileImportExportExtension.cpp
@@ -0,0 +1,10 @@
+﻿// Copyright Out-of-the-Box Plugins 2018-2021. All Rights Reserved.
+
+#include "ToolbarMenuFileImportExportExtension.h"
+
+TArray<FQuickCommandEntry> UToolbarMenuFileImportExportExtension::GetCommands()
+{
+	TArray<FQuickCommandEntry> OutCommands;
+
+	return OutCommands;
+}

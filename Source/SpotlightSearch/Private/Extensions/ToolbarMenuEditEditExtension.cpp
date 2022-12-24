e17b7d0245048b584cc4361d36b3d98edf88b3ab
Oprea Alexandru <oprea_alexandrum@yahoo.com>
2022-12-24 20:03:17 +0200

    base classes for edit->edit

---
+++ Source/SpotlightSearch/Private/Extensions/ToolbarMenuEditEditExtension.cpp
@@ -0,0 +1,10 @@
+﻿// Copyright Out-of-the-Box Plugins 2018-2021. All Rights Reserved.
+
+#include "ToolbarMenuEditEditExtension.h"
+
+TArray<FQuickCommandEntry> UToolbarMenuEditEditExtension::GetCommands()
+{
+	TArray<FQuickCommandEntry> OutCommands;
+
+	return OutCommands;
+}

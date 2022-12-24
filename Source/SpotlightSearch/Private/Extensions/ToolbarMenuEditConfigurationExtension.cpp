51891e647ab8f8f303401015748941cc42529cc6
Oprea Alexandru <oprea_alexandrum@yahoo.com>
2022-12-24 21:41:51 +0200

    base classes for configuration

---
+++ Source/SpotlightSearch/Private/Extensions/ToolbarMenuEditConfigurationExtension.cpp
@@ -0,0 +1,10 @@
+﻿// Copyright Out-of-the-Box Plugins 2018-2021. All Rights Reserved.
+
+#include "ToolbarMenuEditConfigurationExtension.h"
+
+TArray<FQuickCommandEntry> UToolbarMenuEditConfigurationExtension::GetCommands()
+{
+	TArray<FQuickCommandEntry> OutCommands;
+
+	return OutCommands;
+}

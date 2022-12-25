44cbf046933491d34be93e4efb32e526bdbed841
Oprea Alexandru <oprea_alexandrum@yahoo.com>
2022-12-25 07:10:19 +0200

    added open settings extension

---
+++ Source/SpotlightSearch/Private/Extensions/OpenSettingsExtension.h
@@ -0,0 +1,18 @@
+﻿// Copyright Out-of-the-Box Plugins 2018-2023. All Rights Reserved.
+
+#pragma once
+
+#include "QuickMenuExtension.h"
+
+#include "OpenSettingsExtension.generated.h"
+
+/**
+ * Add entries from the main frame toolbar -> Edit menu -> Configuration section
+ */
+UCLASS()
+class UOpenSettingsExtension : public UQuickMenuExtension
+{
+	GENERATED_BODY()
+
+	virtual TArray<FQuickCommandEntry> GetCommands() override;
+};

c4167babf4f2478f1005f1de6d3daa2ee355cfa1
Oprea Alexandru <oprea_alexandrum@yahoo.com>
2022-12-25 15:29:08 +0200

    camera shake previewer added

---
+++ Source/SpotlightSearch/Private/Extensions/ToolbarMenuWindowLevelEditorExtension.cpp
@@ -0,0 +1,30 @@
+﻿// Copyright Out-of-the-Box Plugins 2018-2023. All Rights Reserved.
+
+#include "ToolbarMenuWindowLevelEditorExtension.h"
+
+#include <LevelEditor.h>
+
+#define LOCTEXT_NAMESPACE "QuickActions"
+
+TArray<FQuickCommandEntry> UToolbarMenuWindowLevelEditorExtension::GetCommands()
+{
+	TArray<FQuickCommandEntry> OutCommands;
+
+	FQuickCommandEntry CameraShakePreviewer;
+	CameraShakePreviewer.Title = LOCTEXT("CameraShakePreviewer", "Camera Shake Previewer");
+	CameraShakePreviewer.Tooltip = LOCTEXT("CameraShakePreviewerTooltipText", "Open the camera shake preview panel.");
+	CameraShakePreviewer.Icon = FSlateIcon(FAppStyle::GetAppStyleSetName(), "LevelViewport.ToggleActorPilotCameraView");
+	CameraShakePreviewer.ExecuteCallback = FSimpleDelegate::CreateLambda(
+		[]()
+		{
+			const FLevelEditorModule& LevelEditorModule = FModuleManager::GetModuleChecked<FLevelEditorModule>("LevelEditor");
+			const TSharedPtr<FTabManager> LevelEditorTabManager = LevelEditorModule.GetLevelEditorTabManager();
+			LevelEditorTabManager->TryInvokeTab(FTabId(TEXT("CameraShakePreviewer")));
+		}
+	);
+	OutCommands.Add(CameraShakePreviewer);
+
+	return OutCommands;
+}
+
+#undef LOCTEXT_NAMESPACE

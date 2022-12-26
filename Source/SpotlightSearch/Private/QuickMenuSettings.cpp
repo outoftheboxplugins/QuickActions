61bbecc2ae9dbf958b9a8205e689f49f9eeb33e3
Oprea Alexandru <oprea_alexandrum@yahoo.com>
2022-12-26 21:20:34 +0200

    settings improvements

--- Source/SpotlightSearch/Private/QuickMenuSettings.cpp
+++ Source/SpotlightSearch/Private/QuickMenuSettings.cpp
@@ -1,3 +1,26 @@
 ﻿// Copyright Out-of-the-Box Plugins 2018-2023. All Rights Reserved.
 
 #include "QuickMenuSettings.h"
+
+FName UQuickMenuSettings::GetContainerName() const
+{
+	return TEXT("Editor");
+}
+
+FName UQuickMenuSettings::GetCategoryName() const
+{
+	return TEXT("Out-of-the-Box Plugins");
+}
+
+FName UQuickMenuSettings::GetSectionName() const
+{
+	return TEXT("Quick Menu");
+}
+
+#if WITH_EDITOR
+FText UQuickMenuSettings::GetSectionText() const
+{
+	const FName DisplaySectionName = GetSectionName();
+	return FText::FromName(DisplaySectionName);
+}
+#endif

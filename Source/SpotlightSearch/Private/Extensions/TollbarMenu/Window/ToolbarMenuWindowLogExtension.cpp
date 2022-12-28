f5a2eeacb407dd9eca5a7ca719fa81c8e15e5218
Oprea Alexandru <oprea_alexandrum@yahoo.com>
2022-12-28 22:58:38 +0200

    started work on log category

---
+++ Source/SpotlightSearch/Private/Extensions/TollbarMenu/Window/ToolbarMenuWindowLogExtension.cpp
@@ -0,0 +1,26 @@
+﻿// Copyright Out-of-the-Box Plugins 2018-2023. All Rights Reserved.
+
+#include "ToolbarMenuWindowLogExtension.h"
+
+TArray<FQuickCommandEntry> UToolbarMenuWindowLogExtension::GetCommands()
+{
+	TArray<FQuickCommandEntry> OutCommands;
+
+	FQuickCommandEntry DeviceOutputLog;
+	DeviceOutputLog.Title = NSLOCTEXT("OutputLog", "DeviceOutputLogTab", "Device Output Log");
+	DeviceOutputLog.Tooltip = NSLOCTEXT("OutputLog", "DeviceOutputLogTooltipText", "Open the Device Output Log tab.");
+	DeviceOutputLog.Icon = FSlateIcon(FName("OutputLogStyle"), "Log.TabIcon");
+	DeviceOutputLog.ExecuteCallback = FSimpleDelegate::CreateLambda(
+		[]()
+		{
+			FGlobalTabmanager::Get()->TryInvokeTab(FTabId(TEXT("DeviceOutputLog")));
+		}
+	);
+	OutCommands.Add(DeviceOutputLog);
+
+	FQuickCommandEntry InterchangeResultsBrowser;
+
+	OutCommands.Add(InterchangeResultsBrowser);
+
+	return OutCommands;
+}

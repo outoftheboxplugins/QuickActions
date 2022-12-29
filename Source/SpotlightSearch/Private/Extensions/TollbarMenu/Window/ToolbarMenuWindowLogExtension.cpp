b2281c93f60a6b43fbb0f422809279db171c8000
Oprea Alexandru <oprea_alexandrum@yahoo.com>
2022-12-29 08:51:29 +0200

    added all Log entries

--- Source/SpotlightSearch/Private/Extensions/TollbarMenu/Window/ToolbarMenuWindowLogExtension.cpp
+++ Source/SpotlightSearch/Private/Extensions/TollbarMenu/Window/ToolbarMenuWindowLogExtension.cpp
@@ -2,6 +2,8 @@
 
 #include "ToolbarMenuWindowLogExtension.h"
 
+#include "Styling/SlateIconFinder.h"
+
 TArray<FQuickCommandEntry> UToolbarMenuWindowLogExtension::GetCommands()
 {
 	TArray<FQuickCommandEntry> OutCommands;
@@ -19,8 +21,40 @@ TArray<FQuickCommandEntry> UToolbarMenuWindowLogExtension::GetCommands()
 	OutCommands.Add(DeviceOutputLog);
 
 	FQuickCommandEntry InterchangeResultsBrowser;
-
+	InterchangeResultsBrowser.Title = NSLOCTEXT("InterchangeResultsBrowser", "InterchangeResultsBrowser", "Interchange Results Browser");
+	InterchangeResultsBrowser.Tooltip = NSLOCTEXT("InterchangeResultsBrowser", "InterchangeResultsBrowserTooltipText", "Open the Interchange Results Browser tab.");
+	InterchangeResultsBrowser.Icon = FSlateIconFinder::FindIcon("InterchangeResultsBrowser.TabIcon");
+	InterchangeResultsBrowser.ExecuteCallback = FSimpleDelegate::CreateLambda(
+		[]()
+		{
+			FGlobalTabmanager::Get()->TryInvokeTab(FTabId(TEXT("InterchangeResults")));
+		}
+	);
 	OutCommands.Add(InterchangeResultsBrowser);
+
+	FQuickCommandEntry MessageLog;
+	MessageLog.Title = NSLOCTEXT("UnrealEditor", "MessageLogTab", "Message Log");
+	MessageLog.Tooltip = NSLOCTEXT("UnrealEditor", "MessageLogTooltipText", "Open the Message Log tab.");
+	MessageLog.Icon = FSlateIcon(FAppStyle::GetAppStyleSetName(), "MessageLog.TabIcon");
+	MessageLog.ExecuteCallback = FSimpleDelegate::CreateLambda(
+		[]()
+		{
+			FGlobalTabmanager::Get()->TryInvokeTab(FTabId(TEXT("MessageLog")));
+		}
+	);
+	OutCommands.Add(MessageLog);
+
+	FQuickCommandEntry OutputLog;
+	OutputLog.Title = NSLOCTEXT("OutputLog", "OutputLogTab", "Output Log");
+	OutputLog.Tooltip = NSLOCTEXT("OutputLog", "OutputLogTooltipText", "Open the Output Log tab.");
+	OutputLog.Icon = FSlateIcon(FName("OutputLogStyle"), "Log.TabIcon");
+	OutputLog.ExecuteCallback = FSimpleDelegate::CreateLambda(
+		[]()
+		{
+			FGlobalTabmanager::Get()->TryInvokeTab(FTabId(TEXT("OutputLog")));
+		}
+	);
+	OutCommands.Add(OutputLog);
 
 	return OutCommands;
 }

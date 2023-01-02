d517330eea1de4a90ebaaca0455154c7f7f1e161
Oprea Alexandru <oprea_alexandrum@yahoo.com>
2023-01-02 16:37:34 +0200

    temp before refactor

--- Source/SpotlightSearch/Private/Extensions/TollbarMenu/Window/ToolbarMenuWindowLayoutExtension.cpp
+++ Source/SpotlightSearch/Private/Extensions/TollbarMenu/Window/ToolbarMenuWindowLayoutExtension.cpp
@@ -4,5 +4,49 @@
 
 TArray<TSharedPtr<FQuickCommandEntry>> UToolbarMenuWindowLayoutExtension::GetCommands(const FToolMenuContext& Context)
 {
-	return Super::GetCommands(Context);
+	TArray<TSharedPtr<FQuickCommandEntry>> OutCommands;
+
+	UToolMenus* ToolMenus = UToolMenus::Get();
+	UToolMenu* MainTabFileMenu = ToolMenus->ExtendMenu("MainFrame.MainMenu.Window");
+	FToolMenuSection* WindowLayout = MainTabFileMenu->FindSection("WindowLayout");
+
+	for (FToolMenuEntry& Block : WindowLayout->Blocks)
+	{
+		if (Block.IsSubMenu())
+		{
+			Block.SubMenuData.ConstructMenu.NewToolMenu
+		}
+		TSharedPtr<const FUICommandList> OutCommandsList;
+		if (const FUIAction* FoundAction = Block.GetActionForCommand(Context, OutCommandsList))
+		{
+			TSharedPtr<FQuickCommandEntry> MenuEntry = MakeShared<FQuickCommandEntry>();
+			MenuEntry->Title = Block.Label;
+			MenuEntry->Tooltip = Block.ToolTip;
+			MenuEntry->Icon = Block.Icon;
+			MenuEntry->CanExecuteCallback = FoundAction->CanExecuteAction;
+			MenuEntry->ExecuteCallback = FoundAction->ExecuteAction;
+
+			OutCommands.Emplace(MenuEntry);
+		}
+	}
+
+
+	// FToolMenuSection* UserLayout = MainTabFileMenu->FindSection("UserDefaultLayouts");
+	// for (FToolMenuEntry& Block : UserLayout->Blocks)
+	//{
+	//	TSharedPtr<const FUICommandList> OutCommandsList;
+	//	if (const FUIAction* FoundAction = Block.GetActionForCommand(Context, OutCommandsList))
+	//	{
+	//		TSharedPtr<FQuickCommandEntry> MenuEntry = MakeShared<FQuickCommandEntry>();
+	//		MenuEntry->Title = Block.Label;
+	//		MenuEntry->Tooltip = Block.ToolTip;
+	//		MenuEntry->Icon = Block.Icon;
+	//		MenuEntry->CanExecuteCallback = FoundAction->CanExecuteAction;
+	//		MenuEntry->ExecuteCallback = FoundAction->ExecuteAction;
+
+	//		OutCommands.Emplace(MenuEntry);
+	//	}
+	//}
+
+	return OutCommands;
 }

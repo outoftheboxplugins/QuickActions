a2da59b481fbcf6f53b944873f27d35d40aefbd8
Oprea Alexandru <oprea_alexandrum@yahoo.com>
2023-01-02 19:05:32 +0200

    added classes for Tools->Tools section

---
+++ Source/SpotlightSearch/Private/Extensions/TollbarMenu/Tools/ToolbarMenuToolsToolsExtension.cpp
@@ -0,0 +1,10 @@
+﻿// Copyright Out-of-the-Box Plugins 2018-2023. All Rights Reserved.
+
+#include "ToolbarMenuToolsToolsExtension.h"
+
+TArray<TSharedPtr<FQuickCommandEntry>> UToolbarMenuToolsToolsExtension::GetCommands(const FToolMenuContext& Context)
+{
+	TArray<TSharedPtr<FQuickCommandEntry>> OutCommands;
+
+	return OutCommands;
+}

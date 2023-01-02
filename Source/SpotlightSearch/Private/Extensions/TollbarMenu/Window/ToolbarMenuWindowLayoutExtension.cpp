134d93933b88673d3848058885599cc4de9745de
Oprea Alexandru <oprea_alexandrum@yahoo.com>
2023-01-02 13:22:40 +0200

    added class for layout

---
+++ Source/SpotlightSearch/Private/Extensions/TollbarMenu/Window/ToolbarMenuWindowLayoutExtension.cpp
@@ -0,0 +1,8 @@
+﻿// Copyright Out-of-the-Box Plugins 2018-2023. All Rights Reserved.
+
+#include "ToolbarMenuWindowLayoutExtension.h"
+
+TArray<TSharedPtr<FQuickCommandEntry>> UToolbarMenuWindowLayoutExtension::GetCommands(const FToolMenuContext& Context)
+{
+	return Super::GetCommands(Context);
+}

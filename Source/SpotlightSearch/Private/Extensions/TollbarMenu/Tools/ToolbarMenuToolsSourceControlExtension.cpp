865890a6377c5ce06fc8437f277e0ab9f91ce6d4
Oprea Alexandru <oprea_alexandrum@yahoo.com>
2023-01-03 09:26:59 +0200

    started work on source control integration

---
+++ Source/SpotlightSearch/Private/Extensions/TollbarMenu/Tools/ToolbarMenuToolsSourceControlExtension.cpp
@@ -0,0 +1,22 @@
+﻿// Copyright Out-of-the-Box Plugins 2018-2023. All Rights Reserved.
+
+#include "ToolbarMenuToolsSourceControlExtension.h"
+
+TArray<TSharedPtr<FQuickCommandEntry>> UToolbarMenuToolsSourceControlExtension::GetCommands(const FToolMenuContext& Context)
+{
+	TArray<TSharedPtr<FQuickCommandEntry>> OutCommands;
+
+	const TSharedPtr<FQuickCommandEntry> ViewChangelists = MakeShared<FQuickCommandEntry>();
+	ViewChangelists->Title = NSLOCTEXT("SourceControlWindows", "ChangelistsTabTitle", "View Changelists");
+	ViewChangelists->Tooltip = NSLOCTEXT("SourceControlWindows", "ChangelistsTabTooltip", "Opens a dialog displaying current changelists.");
+	ViewChangelists->Icon = FSlateIcon(FAppStyle::GetAppStyleSetName(), "SourceControl.ChangelistsTab");
+	ViewChangelists->ExecuteCallback = FSimpleDelegate::CreateLambda(
+		[]()
+		{
+			FGlobalTabmanager::Get()->TryInvokeTab(FTabId(TEXT("SourceControlChangelists")));
+		}
+	);
+	OutCommands.Add(ViewChangelists);
+
+	return OutCommands;
+}
---
+++ Source/SpotlightSearch/Private/Extensions/TollbarMenu/Tools/ToolbarMenuToolsSourceControlExtension.h
@@ -0,0 +1,18 @@
+﻿// Copyright Out-of-the-Box Plugins 2018-2023. All Rights Reserved.
+
+#pragma once
+
+#include "QuickMenuExtension.h"
+
+#include "ToolbarMenuToolsSourceControlExtension.generated.h"
+
+/**
+ * Add entries from the main frame toolbar -> Tools menu -> SourceControl section
+ */
+UCLASS()
+class UToolbarMenuToolsSourceControlExtension : public UQuickMenuExtension 
+{
+	GENERATED_BODY()
+
+	virtual TArray<TSharedPtr<FQuickCommandEntry>> GetCommands(const FToolMenuContext& Context) override;
+};

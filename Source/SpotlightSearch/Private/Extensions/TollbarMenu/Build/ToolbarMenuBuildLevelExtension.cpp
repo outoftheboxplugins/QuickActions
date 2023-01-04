ce06456ea92e5bd48c405f292a5c184b030f96e2
Oprea Alexandru <oprea_alexandrum@yahoo.com>
2023-01-04 09:17:03 +0200

    added class to gather build->level section

---
+++ Source/SpotlightSearch/Private/Extensions/TollbarMenu/Build/ToolbarMenuBuildLevelExtension.cpp
@@ -0,0 +1,8 @@
+﻿// Copyright Out-of-the-Box Plugins 2018-2023. All Rights Reserved.
+
+#include "ToolbarMenuBuildLevelExtension.h"
+
+TArray<TSharedPtr<FQuickCommandEntry>> UToolbarMenuBuildLevelExtension::GetCommands(const FToolMenuContext& Context)
+{
+	return Super::GetCommands(Context);
+}

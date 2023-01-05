21744466557f667f36edd609c996fa0e4ef4e394
Oprea Alexandru <oprea_alexandrum@yahoo.com>
2023-01-05 10:32:51 +0200

    add reflection gatherer

---
+++ Source/SpotlightSearch/Private/Extensions/ReflectionFunctionsExtension.h
@@ -0,0 +1,22 @@
+﻿// Copyright Out-of-the-Box Plugins 2018-2023. All Rights Reserved.
+
+#pragma once
+
+#include "QuickMenuExtension.h"
+
+#include "ReflectionFunctionsExtension.generated.h"
+
+/**
+ * TODO:
+ */
+UCLASS()
+class UReflectionFunctionsExtension : public UQuickMenuExtension
+{
+	GENERATED_BODY()
+
+	virtual TArray<TSharedPtr<FQuickCommandEntry>> GetCommands(const FToolMenuContext& Context) override;
+
+	// Example tooltip
+	UFUNCTION(meta = (QuickActionEntry))
+	void ExampleFunction();
+};

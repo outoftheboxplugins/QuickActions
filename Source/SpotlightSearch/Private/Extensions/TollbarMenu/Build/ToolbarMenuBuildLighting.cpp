9308c469e2dfbfb464493c044d7610eb8281671f
Oprea Alexandru <oprea_alexandrum@yahoo.com>
2023-01-07 13:56:42 +0200

    added all build lighting commands

--- Source/SpotlightSearch/Private/Extensions/TollbarMenu/Build/ToolbarMenuBuildLighting.cpp
+++ Source/SpotlightSearch/Private/Extensions/TollbarMenu/Build/ToolbarMenuBuildLighting.cpp
@@ -47,7 +47,7 @@ TArray<TSharedPtr<FQuickCommandEntry>> UToolbarMenuBuildLighting::GetCommands(co
 	}
 
 	const TSharedPtr<FQuickCommandEntry> LightDensityRenderGreyscale = MakeShared<FQuickCommandEntry>(FLevelEditorCommands::Get().LightingDensity_RenderGrayscale.ToSharedRef(), LevelEditorCommands);
-	const FText RenderGreyscaleAction = FLevelEditorActionCallbacks::IsLightingDensityRenderGrayscaleChecked() ? LOCTEXT("Enable", "Enable:") : LOCTEXT("Disable", "Disable:");
+	const FText RenderGreyscaleAction = FLevelEditorActionCallbacks::IsLightingDensityRenderGrayscaleChecked() ? LOCTEXT("Disable", "Disable:") : LOCTEXT("Enable", "Enable:");
 	LightDensityRenderGreyscale->Title = FText::Format(INVTEXT("{0} {1}"), RenderGreyscaleAction, LightDensityRenderGreyscale->Title.Get());
 	OutCommands.Emplace(LightDensityRenderGreyscale);
 
@@ -64,14 +64,24 @@ TArray<TSharedPtr<FQuickCommandEntry>> UToolbarMenuBuildLighting::GetCommands(co
 	}
 
 	const TSharedPtr<FQuickCommandEntry> LightResSelectOnly = MakeShared<FQuickCommandEntry>(FLevelEditorCommands::Get().LightingResolution_SelectedObjectsOnly.ToSharedRef(), LevelEditorCommands);
-	const FText SelectedOnlyAction = FLevelEditorActionCallbacks::IsLightingResolutionSelectedObjectsOnlyChecked() ? LOCTEXT("Enable", "Enable:") : LOCTEXT("Disable", "Disable:");
+	const FText SelectedOnlyAction = !FLevelEditorActionCallbacks::IsLightingResolutionSelectedObjectsOnlyChecked() ? LOCTEXT("Disable", "Disable:") : LOCTEXT("Enable", "Enable:");
 	LightResSelectOnly->Title = FText::Format(INVTEXT("{0} {1}"), SelectedOnlyAction, LightResSelectOnly->Title.Get());
 	OutCommands.Emplace(LightResSelectOnly);
 
 	const TSharedPtr<FQuickCommandEntry> LightningStaticMeshInfo = MakeShared<FQuickCommandEntry>(FLevelEditorCommands::Get().LightingStaticMeshInfo.ToSharedRef(), LevelEditorCommands);
 	OutCommands.Emplace(LightningStaticMeshInfo);
 
-	// TODO: Add Use Error coloring button
+	const TSharedPtr<FQuickCommandEntry> LightBuildErrorColoring = MakeShared<FQuickCommandEntry>(FLevelEditorCommands::Get().LightingBuildOptions_UseErrorColoring.ToSharedRef(), LevelEditorCommands);
+	const FText ErrorColoringAction = FLevelEditorActionCallbacks::LightingBuildOptions_UseErrorColoring_IsChecked() ? LOCTEXT("Disable", "Disable:") : LOCTEXT("Enable", "Enable:");
+	LightBuildErrorColoring->Title = FText::Format(INVTEXT("{0} {1}"), ErrorColoringAction, LightBuildErrorColoring->Title.Get());
+	OutCommands.Emplace(LightBuildErrorColoring);
+
+	const TSharedPtr<FQuickCommandEntry> LightBuildShowStats = MakeShared<FQuickCommandEntry>(FLevelEditorCommands::Get().LightingBuildOptions_ShowLightingStats.ToSharedRef(), LevelEditorCommands);
+	const FText ShowStatsAction = FLevelEditorActionCallbacks::LightingBuildOptions_ShowLightingStats_IsChecked() ? LOCTEXT("Disable", "Disable:") : LOCTEXT("Enable", "Enable:");
+	LightBuildShowStats->Title = FText::Format(INVTEXT("{0} {1}"), ShowStatsAction, LightBuildShowStats->Title.Get());
+	OutCommands.Emplace(LightBuildShowStats);
+
+	TSharedPtr<FUICommandInfo> LightingBuildOptions_ShowLightingStats;
 	return OutCommands;
 }
 

1a494b3cc1dcbbfb04a1ae6b4e5c80de79fa0b9f
Oprea Alexandru <oprea_alexandrum@yahoo.com>
2023-01-08 15:15:53 +0200

    added more light buttons

---
+++ Source/SpotlightSearch/Private/Extensions/TollbarMenu/Build/ToolbarMenuBuildLandscapeExtension.h
@@ -0,0 +1,18 @@
+﻿// Copyright Out-of-the-Box Plugins 2018-2023. All Rights Reserved.
+
+#pragma once
+
+#include "QuickMenuExtension.h"
+
+#include "ToolbarMenuBuildLandscapeExtension.generated.h"
+
+/**
+ * Add entries from the main frame toolbar -> Build menu -> Landscape section
+ */
+UCLASS()
+class UToolbarMenuBuildLandscapeExtension : public UQuickMenuExtension
+{
+	GENERATED_BODY()
+
+	virtual TArray<TSharedPtr<FQuickCommandEntry>> GetCommands(const FToolMenuContext& Context) override;
+};

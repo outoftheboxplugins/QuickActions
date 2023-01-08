ab69af6a8ea6e4fe79818cf6b303238b1a928a69
Oprea Alexandru <oprea_alexandrum@yahoo.com>
2023-01-08 15:15:53 +0200

    added more light buttons

---
+++ Source/SpotlightSearch/Private/Extensions/TollbarMenu/Build/ToolbarMenuBuildLandscapeExtension.cpp
@@ -0,0 +1,19 @@
+﻿// Copyright Out-of-the-Box Plugins 2018-2023. All Rights Reserved.
+
+#include "ToolbarMenuBuildLandscapeExtension.h"
+
+#include <LevelEditor.h>
+#include <LevelEditorActions.h>
+
+TArray<TSharedPtr<FQuickCommandEntry>> UToolbarMenuBuildLandscapeExtension::GetCommands(const FToolMenuContext& Context)
+{
+	TArray<TSharedPtr<FQuickCommandEntry>> OutCommands;
+
+	const FLevelEditorModule& LevelEditorModule = FModuleManager::Get().LoadModuleChecked<FLevelEditorModule>("LevelEditor");
+	const TSharedRef<FUICommandList> LevelEditorCommands = LevelEditorModule.GetGlobalLevelEditorActions();
+
+	const TSharedPtr<FQuickCommandEntry> BuildAllLandscape = MakeShared<FQuickCommandEntry>(FLevelEditorCommands::Get().BuildAllLandscape.ToSharedRef(), LevelEditorCommands);
+	OutCommands.Emplace(BuildAllLandscape);
+
+	return OutCommands;
+}

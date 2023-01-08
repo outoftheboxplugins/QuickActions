5e54746e1253700685659015cf17e9c543b5274a
Oprea Alexandru <oprea_alexandrum@yahoo.com>
2023-01-08 15:29:14 +0200

    finished the landscape section

--- Source/SpotlightSearch/Private/Extensions/TollbarMenu/Build/ToolbarMenuBuildLandscapeExtension.cpp
+++ Source/SpotlightSearch/Private/Extensions/TollbarMenu/Build/ToolbarMenuBuildLandscapeExtension.cpp
@@ -2,6 +2,7 @@
 
 #include "ToolbarMenuBuildLandscapeExtension.h"
 
+#include <LandscapeSubsystem.h>
 #include <LevelEditor.h>
 #include <LevelEditorActions.h>
 
@@ -14,6 +15,74 @@ TArray<TSharedPtr<FQuickCommandEntry>> UToolbarMenuBuildLandscapeExtension::GetC
 
 	const TSharedPtr<FQuickCommandEntry> BuildAllLandscape = MakeShared<FQuickCommandEntry>(FLevelEditorCommands::Get().BuildAllLandscape.ToSharedRef(), LevelEditorCommands);
 	OutCommands.Emplace(BuildAllLandscape);
+
+	const TSharedPtr<FQuickCommandEntry> BuildGITextures = MakeShared<FQuickCommandEntry>();
+	BuildGITextures->Title = NSLOCTEXT("LandscapeEditor", "BuildGITexturesOnly", "Build GI Textures Only");
+	BuildGITextures->Tooltip = NSLOCTEXT("LandscapeEditor", "BuildGIBakedTextures ", "Build GI baked base color textures");
+	BuildGITextures->ExecuteCallback = FSimpleDelegate::CreateLambda(
+		[]()
+		{
+			if (const UWorld* World = GEditor->GetEditorWorldContext().World())
+			{
+				if (ULandscapeSubsystem* LandscapeSubsystem = World->GetSubsystem<ULandscapeSubsystem>())
+				{
+					LandscapeSubsystem->BuildGIBakedTextures();
+				}
+			}
+		}
+	);
+	OutCommands.Emplace(BuildGITextures);
+
+	const TSharedPtr<FQuickCommandEntry> BuildGrassMaps = MakeShared<FQuickCommandEntry>();
+	BuildGrassMaps->Title = NSLOCTEXT("LandscapeEditor", "BuildGrassMapsOnly", "Build Grass Maps Only");
+	BuildGrassMaps->Tooltip = NSLOCTEXT("LandscapeEditor", "BuildLandscapeGrassMaps", "Build landscape grass maps");
+	BuildGrassMaps->ExecuteCallback = FSimpleDelegate::CreateLambda(
+		[]()
+		{
+			if (const UWorld* World = GEditor->GetEditorWorldContext().World())
+			{
+				if (ULandscapeSubsystem* LandscapeSubsystem = World->GetSubsystem<ULandscapeSubsystem>())
+				{
+					LandscapeSubsystem->BuildGrassMaps();
+				}
+			}
+		}
+	);
+	OutCommands.Emplace(BuildGrassMaps);
+
+	const TSharedPtr<FQuickCommandEntry> BuildPhysicalMaterial = MakeShared<FQuickCommandEntry>();
+	BuildPhysicalMaterial->Title = NSLOCTEXT("LandscapeEditor", "BuildPhysicalMaterialOnly", "Build Physical Material Only");
+	BuildPhysicalMaterial->Tooltip = NSLOCTEXT("LandscapeEditor", "BuildLandscapePhysicalMaterial", "Build landscape physical material");
+	BuildPhysicalMaterial->ExecuteCallback = FSimpleDelegate::CreateLambda(
+		[]()
+		{
+			if (const UWorld* World = GEditor->GetEditorWorldContext().World())
+			{
+				if (ULandscapeSubsystem* LandscapeSubsystem = World->GetSubsystem<ULandscapeSubsystem>())
+				{
+					LandscapeSubsystem->BuildPhysicalMaterial();
+				}
+			}
+		}
+	);
+	OutCommands.Emplace(BuildPhysicalMaterial);
+
+	const TSharedPtr<FQuickCommandEntry> BuildNanite = MakeShared<FQuickCommandEntry>();
+	BuildNanite->Title = NSLOCTEXT("LandscapeEditor", "BuildNaniteOnly", "Build Nanite Only");
+	BuildNanite->Tooltip = NSLOCTEXT("LandscapeEditor", "BuildLandscapeNanite", "Build Nanite representation");
+	BuildNanite->ExecuteCallback = FSimpleDelegate::CreateLambda(
+		[]()
+		{
+			if (const UWorld* World = GEditor->GetEditorWorldContext().World())
+			{
+				if (ULandscapeSubsystem* LandscapeSubsystem = World->GetSubsystem<ULandscapeSubsystem>())
+				{
+					LandscapeSubsystem->BuildNanite();
+				}
+			}
+		}
+	);
+	OutCommands.Emplace(BuildNanite);
 
 	return OutCommands;
 }

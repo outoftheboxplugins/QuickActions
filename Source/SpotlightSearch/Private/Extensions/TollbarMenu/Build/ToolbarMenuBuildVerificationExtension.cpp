c044fc7a4cab705417b9321289342a164c225ad3
Oprea Alexandru <oprea_alexandrum@yahoo.com>
2023-01-10 09:50:48 +0200

    finished build section

---
+++ Source/SpotlightSearch/Private/Extensions/TollbarMenu/Build/ToolbarMenuBuildVerificationExtension.cpp
@@ -0,0 +1,19 @@
+﻿// Copyright Out-of-the-Box Plugins 2018-2023. All Rights Reserved.
+
+#include "ToolbarMenuBuildVerificationExtension.h"
+
+#include <LevelEditor.h>
+#include <LevelEditorActions.h>
+
+TArray<TSharedPtr<FQuickCommandEntry>> UToolbarMenuBuildVerificationExtension::GetCommands(const FToolMenuContext& Context)
+{
+	TArray<TSharedPtr<FQuickCommandEntry>> OutCommands;
+
+	const FLevelEditorModule& LevelEditorModule = FModuleManager::Get().LoadModuleChecked<FLevelEditorModule>("LevelEditor");
+	const TSharedRef<FUICommandList> LevelEditorCommands = LevelEditorModule.GetGlobalLevelEditorActions();
+
+	const TSharedPtr<FQuickCommandEntry> MapCheck = MakeShared<FQuickCommandEntry>(FLevelEditorCommands::Get().MapCheck.ToSharedRef(), LevelEditorCommands);
+	OutCommands.Emplace(MapCheck);
+
+	return OutCommands;
+}

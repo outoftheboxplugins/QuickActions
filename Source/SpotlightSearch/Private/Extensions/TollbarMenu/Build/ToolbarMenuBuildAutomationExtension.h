d794be53b466150209be58cc5f42ea656c2c9bec
Oprea Alexandru <oprea_alexandrum@yahoo.com>
2023-01-10 09:50:48 +0200

    finished build section

---
+++ Source/SpotlightSearch/Private/Extensions/TollbarMenu/Build/ToolbarMenuBuildAutomationExtension.h
@@ -0,0 +1,18 @@
+﻿// Copyright Out-of-the-Box Plugins 2018-2023. All Rights Reserved.
+
+#pragma once
+
+#include "QuickMenuExtension.h"
+
+#include "ToolbarMenuBuildAutomationExtension.generated.h"
+
+/**
+ * Add entries from the main frame toolbar -> Build menu -> Automation section
+ */
+UCLASS()
+class UToolbarMenuBuildAutomationExtension : public UQuickMenuExtension
+{
+	GENERATED_BODY()
+
+	virtual TArray<TSharedPtr<FQuickCommandEntry>> GetCommands(const FToolMenuContext& Context) override;
+};

ab2b04c16a9d9b22514548910e73a81ca51a33cb
Oprea Alexandru <oprea_alexandrum@yahoo.com>
2023-01-14 21:09:11 +0200

    added Help -> Reference section

---
+++ Source/SpotlightSearch/Private/Extensions/TollbarMenu/Help/ToolbarMenuHelpReferenceExtension.cpp
@@ -0,0 +1,30 @@
+﻿// Copyright Out-of-the-Box Plugins 2018-2023. All Rights Reserved.
+
+#include "ToolbarMenuHelpReferenceExtension.h"
+
+TArray<TSharedPtr<FQuickCommandEntry>> UToolbarMenuHelpReferenceExtension::GetCommands(const FToolMenuContext& Context)
+{
+	TArray<TSharedPtr<FQuickCommandEntry>> OutCommands;
+
+	UToolMenus* ToolMenus = UToolMenus::Get();
+	UToolMenu* FileMenu = ToolMenus->FindMenu("MainFrame.MainMenu.Help");
+
+	FToolMenuSection* ReferenceSection = FileMenu->FindSection("Reference");
+	for (FToolMenuEntry& Block : ReferenceSection->Blocks)
+	{
+		TSharedPtr<const FUICommandList> OutCommandsList;
+		if (const FUIAction* FoundAction = Block.GetActionForCommand(Context, OutCommandsList))
+		{
+			const TSharedPtr<FQuickCommandEntry> MenuEntry = MakeShared<FQuickCommandEntry>();
+			MenuEntry->Title = Block.Label;
+			MenuEntry->Tooltip = Block.ToolTip;
+			MenuEntry->Icon = Block.Icon;
+			MenuEntry->CanExecuteCallback = FoundAction->CanExecuteAction;
+			MenuEntry->ExecuteCallback = FoundAction->ExecuteAction;
+
+			OutCommands.Emplace(MenuEntry);
+		}
+	}
+	
+	return OutCommands;
+}

74fb24e264aac90d63076a4207424d25631f3d0e
Oprea Alexandru <oprea_alexandrum@yahoo.com>
2023-01-15 11:01:12 +0200

    added class structure for Select -> General section

---
+++ Source/SpotlightSearch/Private/Extensions/TollbarMenu/Select/ToolbarMenuSelectGeneralExtension.cpp
@@ -0,0 +1,12 @@
+﻿// Copyright Out-of-the-Box Plugins 2018-2023. All Rights Reserved.
+
+#include "ToolbarMenuSelectGeneralExtension.h"
+
+#include <LevelEditorActions.h>
+
+TArray<TSharedPtr<FQuickCommandEntry>> UToolbarMenuSelectGeneralExtension::GetCommands(const FToolMenuContext& Context)
+{
+	TArray<TSharedPtr<FQuickCommandEntry>> OutCommands;
+
+	return OutCommands;
+}

6595e2c6ad7c6c97b0051b5ab23964577bc00bd0
Oprea Alexandru <oprea_alexandrum@yahoo.com>
2023-04-01 20:18:04 +0300

    renamed listview to match purpose + documenting

---
+++ Source/SpotlightSearch/Private/SNonFocusingListView.h
@@ -0,0 +1,20 @@
+﻿// Copyright Out-of-the-Box Plugins 2018-2023. All Rights Reserved.
+
+#pragma once
+
+/**
+ * @brief ListView which does not focus a widget if we are scrolling to.
+ */
+template <typename ItemType>
+class SNonFocusingListView final : public SListView<ItemType>
+{
+	// Begin SListView interface
+	virtual void NotifyItemScrolledIntoView() override
+	{
+		// We want to avoid focusing the widget we scrolled to, because the search bar should keep the focus at all times.
+		this->bNavigateOnScrollIntoView = false;
+
+		SListView<ItemType>::NotifyItemScrolledIntoView();
+	}
+	// End SListView interface
+};
--- Source/SpotlightSearch/Private/SQuickCommandsListView.cpp
+++
@@ -1,9 +0,0 @@
-﻿#include "SQuickCommandsListView.h"
-
-void SQuickCommandsListView::NotifyItemScrolledIntoView()
-{
-	// We want to avoid focusing the widget we scrolled to, because the search bar should keep the focus at all times.
-	bNavigateOnScrollIntoView = false;
-
-	SListView::NotifyItemScrolledIntoView();
-}

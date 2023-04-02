e1f2e7d0856044a22456a3dae8528d8426ad98a9
Oprea Alexandru <oprea_alexandrum@yahoo.com>
2023-04-02 11:52:07 +0300

    reorder functions to match .h

--- Source/SpotlightSearch/Private/SQuickMenuWindow.cpp
+++ Source/SpotlightSearch/Private/SQuickMenuWindow.cpp
@@ -12,7 +12,6 @@
 #define LOCTEXT_NAMESPACE "FQuickMenuModule"
 
 // TODO: Show this menu in the middle of the Unreal screen instead of middle of the monitor
-// TODO: Reorder functions in here to match the header file order
 
 namespace
 {
@@ -21,80 +20,6 @@ namespace
 		return (i % n + n) % n;
 	}
 } // namespace
-
-void SQuickMenuWindow::OnFilterTextChanged(const FText& Text)
-{
-	FilteredCommands.Empty();
-
-	TArray<FQuickMenuItem> Commands;
-	const UQuickMenuDiscoverySubsystem* DiscoverySubsystem = GEditor->GetEditorSubsystem<UQuickMenuDiscoverySubsystem>();
-	TArray<TSharedPtr<FQuickCommandEntry>> AllEntries = DiscoverySubsystem->GetAllCommands();
-	for (const TSharedPtr<FQuickCommandEntry>& Entry : AllEntries)
-	{
-		Commands.Emplace(Entry.ToSharedRef());
-	}
-
-	TArray<FQuickMenuItem> AvailableCommands = Commands;
-
-	if (Text.IsEmpty())
-	{
-		const UQuickMenuSettings* Settings = GetDefault<UQuickMenuSettings>();
-		const TArray<FString>& RecentCommands = Settings->GetRecentCommands();
-
-		for (const FString& Command : RecentCommands)
-		{
-			const FQuickMenuItem* FoundCommand = AvailableCommands.FindByPredicate(
-				[Command](const FQuickMenuItem& Entry)
-				{
-					return Entry->GetUniqueCommandName() == Command;
-				}
-			);
-
-			if (FoundCommand)
-			{
-				FilteredCommands.Emplace(*FoundCommand);
-			}
-		}
-	}
-	else
-	{
-		// First get all the abbreviations
-		for (auto It = AvailableCommands.CreateIterator(); It; ++It)
-		{
-			FQuickMenuItem Command = *It;
-			if (QuickMenuHelpers::IsAbbreviation(Command->GetUniqueCommandName(), Text.ToString()))
-			{
-				FilteredCommands.Add(*It);
-				It.RemoveCurrent();
-			}
-		}
-
-		// Second get the perfect matches
-		for (auto It = AvailableCommands.CreateIterator(); It; ++It)
-		{
-			FQuickMenuItem Command = *It;
-			if (QuickMenuHelpers::IsStartingWith(Command->GetUniqueCommandName(), Text.ToString()))
-			{
-				FilteredCommands.Add(*It);
-				It.RemoveCurrent();
-			}
-		}
-
-		// Third get fuzzy entries
-		for (auto It = AvailableCommands.CreateIterator(); It; ++It)
-		{
-			FQuickMenuItem Command = *It;
-			if (QuickMenuHelpers::IsCloseTo(Command->GetUniqueCommandName(), Text.ToString()))
-			{
-				FilteredCommands.Add(*It);
-				It.RemoveCurrent();
-			}
-		}
-	}
-
-	ListView->RequestListRefresh();
-	UpdateSelection(0);
-}
 
 void SQuickMenuWindow::Construct(const FArguments& InArgs)
 {
@@ -175,6 +100,140 @@ void SQuickMenuWindow::Construct(const FArguments& InArgs)
 	RegisterActiveTimer(0.0f, FWidgetActiveTimerDelegate::CreateSP(this, &SQuickMenuWindow::SetFocusPostConstruct));
 }
 
+bool SQuickMenuWindow::OnIsActiveChanged(const FWindowActivateEvent& ActivateEvent)
+{
+	if (ActivateEvent.GetActivationType() == FWindowActivateEvent::EA_Deactivate)
+	{
+		CloseWindow();
+	}
+
+	return SWindow::OnIsActiveChanged(ActivateEvent);
+}
+
+FReply SQuickMenuWindow::OnPreviewKeyDown(const FGeometry& MyGeometry, const FKeyEvent& InKeyEvent)
+{
+	if (InKeyEvent.GetKey() == EKeys::Enter)
+	{
+		ConfirmSelection();
+		return FReply::Handled();
+	}
+	if (InKeyEvent.GetKey() == EKeys::Escape)
+	{
+		CloseWindow();
+		return FReply::Handled();
+	}
+
+	return SWindow::OnPreviewKeyDown(MyGeometry, InKeyEvent);
+}
+
+EActiveTimerReturnType SQuickMenuWindow::SetFocusPostConstruct(double InCurrentTime, float InDeltaTime)
+{
+	if (EditableText.IsValid())
+	{
+		FWidgetPath WidgetToFocusPath;
+		FSlateApplication::Get().GeneratePathToWidgetUnchecked(EditableText.ToSharedRef(), WidgetToFocusPath);
+		FSlateApplication::Get().SetKeyboardFocus(WidgetToFocusPath, EFocusCause::SetDirectly);
+		WidgetToFocusPath.GetWindow()->SetWidgetToFocusOnActivate(EditableText);
+
+		// Trigger a text change to filter the selection for an empty search
+		OnFilterTextChanged(FText::GetEmpty());
+
+		return EActiveTimerReturnType::Stop;
+	}
+
+	return EActiveTimerReturnType::Continue;
+}
+
+FReply SQuickMenuWindow::OnSearchKeyDown(const FGeometry& MyGeometry, const FKeyEvent& InKeyEvent)
+{
+	if (InKeyEvent.GetKey() == EKeys::Down)
+	{
+		UpdateSelection(1);
+		return FReply::Handled();
+	}
+	if (InKeyEvent.GetKey() == EKeys::Up)
+	{
+		UpdateSelection(-1);
+		return FReply::Handled();
+	}
+
+	return FReply::Unhandled();
+}
+
+void SQuickMenuWindow::OnFilterTextChanged(const FText& Text)
+{
+	FilteredCommands.Empty();
+
+	TArray<FQuickMenuItem> Commands;
+	const UQuickMenuDiscoverySubsystem* DiscoverySubsystem = GEditor->GetEditorSubsystem<UQuickMenuDiscoverySubsystem>();
+	TArray<TSharedPtr<FQuickCommandEntry>> AllEntries = DiscoverySubsystem->GetAllCommands();
+	for (const TSharedPtr<FQuickCommandEntry>& Entry : AllEntries)
+	{
+		Commands.Emplace(Entry.ToSharedRef());
+	}
+
+	TArray<FQuickMenuItem> AvailableCommands = Commands;
+
+	if (Text.IsEmpty())
+	{
+		const UQuickMenuSettings* Settings = GetDefault<UQuickMenuSettings>();
+		const TArray<FString>& RecentCommands = Settings->GetRecentCommands();
+
+		for (const FString& Command : RecentCommands)
+		{
+			const FQuickMenuItem* FoundCommand = AvailableCommands.FindByPredicate(
+				[Command](const FQuickMenuItem& Entry)
+				{
+					return Entry->GetUniqueCommandName() == Command;
+				}
+			);
+
+			if (FoundCommand)
+			{
+				FilteredCommands.Emplace(*FoundCommand);
+			}
+		}
+	}
+	else
+	{
+		// First get all the abbreviations
+		for (auto It = AvailableCommands.CreateIterator(); It; ++It)
+		{
+			FQuickMenuItem Command = *It;
+			if (QuickMenuHelpers::IsAbbreviation(Command->GetUniqueCommandName(), Text.ToString()))
+			{
+				FilteredCommands.Add(*It);
+				It.RemoveCurrent();
+			}
+		}
+
+		// Second get the perfect matches
+		for (auto It = AvailableCommands.CreateIterator(); It; ++It)
+		{
+			FQuickMenuItem Command = *It;
+			if (QuickMenuHelpers::IsStartingWith(Command->GetUniqueCommandName(), Text.ToString()))
+			{
+				FilteredCommands.Add(*It);
+				It.RemoveCurrent();
+			}
+		}
+
+		// Third get fuzzy entries
+		for (auto It = AvailableCommands.CreateIterator(); It; ++It)
+		{
+			FQuickMenuItem Command = *It;
+			if (QuickMenuHelpers::IsCloseTo(Command->GetUniqueCommandName(), Text.ToString()))
+			{
+				FilteredCommands.Add(*It);
+				It.RemoveCurrent();
+			}
+		}
+	}
+
+	ListView->RequestListRefresh();
+	UpdateSelection(0);
+}
+
 TSharedRef<ITableRow> SQuickMenuWindow::MakeCommandListItem(FQuickMenuItem Selection, const TSharedRef<STableViewBase>& OwnerTable)
 {
 	const bool bCanExecute = Selection->IsAllowedToExecute();
@@ -237,50 +296,6 @@ TSharedRef<ITableRow> SQuickMenuWindow::MakeCommandListItem(FQuickMenuItem Selec
 	// clang-format on
 }
 
-FReply SQuickMenuWindow::OnSearchKeyDown(const FGeometry& MyGeometry, const FKeyEvent& InKeyEvent)
-{
-	if (InKeyEvent.GetKey() == EKeys::Down)
-	{
-		UpdateSelection(1);
-		return FReply::Handled();
-	}
-	if (InKeyEvent.GetKey() == EKeys::Up)
-	{
-		UpdateSelection(-1);
-		return FReply::Handled();
-	}
-
-	return FReply::Unhandled();
-}
-
-FReply SQuickMenuWindow::OnPreviewKeyDown(const FGeometry& MyGeometry, const FKeyEvent& InKeyEvent)
-{
-	if (InKeyEvent.GetKey() == EKeys::Enter)
-	{
-		ConfirmSelection();
-		return FReply::Handled();
-	}
-	if (InKeyEvent.GetKey() == EKeys::Escape)
-	{
-		CloseWindow();
-		return FReply::Handled();
-	}
-
-	return SWindow::OnPreviewKeyDown(MyGeometry, InKeyEvent);
-}
-
-void SQuickMenuWindow::CloseWindow()
-{
-	// TODO: Prevents this from getting called twice, maybe we should investigate how we can avoid the double call instead.
-	if (bWasClosed)
-	{
-		return;
-	}
-
-	bWasClosed = true;
-	RequestDestroyWindow();
-}
-
 void SQuickMenuWindow::ConfirmSelection()
 {
 	if (FilteredCommands.IsEmpty())
@@ -335,32 +350,16 @@ bool SQuickMenuWindow::ShouldShowDescription() const
 	return bIsAltPressed;
 }
 
-bool SQuickMenuWindow::OnIsActiveChanged(const FWindowActivateEvent& ActivateEvent)
+void SQuickMenuWindow::CloseWindow()
 {
-	if (ActivateEvent.GetActivationType() == FWindowActivateEvent::EA_Deactivate)
+	// TODO: Prevents this from getting called twice, maybe we should investigate how we can avoid the double call instead.
+	if (bWasClosed)
 	{
-		CloseWindow();
+		return;
 	}
 
-	return SWindow::OnIsActiveChanged(ActivateEvent);
-}
-
-EActiveTimerReturnType SQuickMenuWindow::SetFocusPostConstruct(double InCurrentTime, float InDeltaTime)
-{
-	if (EditableText.IsValid())
-	{
-		FWidgetPath WidgetToFocusPath;
-		FSlateApplication::Get().GeneratePathToWidgetUnchecked(EditableText.ToSharedRef(), WidgetToFocusPath);
-		FSlateApplication::Get().SetKeyboardFocus(WidgetToFocusPath, EFocusCause::SetDirectly);
-		WidgetToFocusPath.GetWindow()->SetWidgetToFocusOnActivate(EditableText);
-
-		// Trigger a text change to filter the selection for an empty search
-		OnFilterTextChanged(FText::GetEmpty());
-
-		return EActiveTimerReturnType::Stop;
-	}
-
-	return EActiveTimerReturnType::Continue;
+	bWasClosed = true;
+	RequestDestroyWindow();
 }
 
 #undef LOCTEXT_NAMESPACE
